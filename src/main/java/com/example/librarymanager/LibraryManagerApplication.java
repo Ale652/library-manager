7a5176ad4eb30aa9df467ec3f4032ff6a1c145d4
al moise <mmoisealexandra42@gmail.com>
2023-02-05 18:06:15 +0200

    added hql queries

--- src/main/java/com/example/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/example/librarymanager/LibraryManagerApplication.java
@@ -1,11 +1,14 @@
 package com.example.librarymanager;
 
+import com.example.librarymanager.models.Book;
 import com.example.librarymanager.models.Exemplu;
 import com.example.librarymanager.repository.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 
 @SpringBootApplication
@@ -24,9 +27,22 @@ public class LibraryManagerApplication {
 //                System.out.println(e);
 //            });
 
-            bookRepository.findAllBooksWithLowerPriceAndAuthor(15.8709393,"Maryanne Say")
-                    .get().forEach(System.out::println);
+//            bookRepository.findAllBooksWithLowerPriceAndAuthor(15.8709393,"Maryanne Say")
+//                    .get().forEach(System.out::println);
+
+            System.out.println("############### findLowerPriceBook :");
+            System.out.println(bookRepository.findLowerPriceBook().get());
+            System.out.println("############### findMaxPriceBook :");
+            System.out.println(bookRepository.findMaxPriceBook().get());
+            System.out.println("############### findTop10PriceBooks :");
+            bookRepository.findTop10PriceBooks(PageRequest.of(1,10)).forEach(System.out::println);
+            System.out.println("############### findLower10PriceBooks :");
+            bookRepository.findLower10PriceBooks(PageRequest.of(1, 10)).forEach(System.out::println);
+            System.out.println("############### Best Books :");
+            bookRepository.bestBooks().get().forEach(System.out::println);
 
         };
     }
+
+
 }
--- src/main/java/com/example/librarymanager/models/Book.java
+++ src/main/java/com/example/librarymanager/models/Book.java
@@ -1,6 +1,8 @@
 package com.example.librarymanager.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,14 +30,17 @@ public class Book {
             nullable = false,
             columnDefinition = "TEXT"
     )
+    @Size(min = 2, message = "min title length should be 2")
     String title;
 
+    @Size(min = 2, message = "min author length should be 2")
     @Column( name="author",
             nullable = false)
     String author;
 
     @Column( name="price",
             nullable = false)
+            @DecimalMin(value = "0.1",message = "Price should have a value.")
     double price;
 
     @Column( name="stars")
